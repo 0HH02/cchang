f5b1e3c2a3175539d4f53b4d29b26ffcfaa4c927
Frank Perez <fp848584@gmail.com>
2022-09-15 23:45:24 -0400

    preparing project source code

---
+++ pages/Contact.jsx
@@ -0,0 +1,8 @@
+export default function Contact ()
+{
+    return (
+        <div className="">
+            Esta es la seccion contacto
+        </div>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
-import '../styles/globals.css'
+import '../styles/Desktop.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
 export default MyApp
